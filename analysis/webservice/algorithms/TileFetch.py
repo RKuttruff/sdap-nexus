75bc4f135106721964b39bb0db26176dcf6345fc
rileykk <rileykk@jpl.nasa.gov>
2023-06-29 14:23:30 -0700

    formatting

--- analysis/webservice/algorithms/TileFetch.py
+++ analysis/webservice/algorithms/TileFetch.py
@@ -1,3 +1,19 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 from webservice.NexusHandler import nexus_handler
 from webservice.algorithms.NexusCalcHandler import NexusCalcHandler
 from webservice.webmodel import NexusResults, NexusProcessingException
@@ -39,7 +55,6 @@ class NumpyEncoder(JSONEncoder):
             return float(o)
         else:
             return JSONEncoder.default(self, o)
-
 
 
 @nexus_handler
@@ -112,6 +127,7 @@ class TileFetch(NexusCalcHandler):
 
 class TileFetchResults(NexusResults):
     def __init__(self, tiles, ids):
+        NexusResults.__init__(self)
         self.__ids = ids
         self.__tiles = tiles
 
